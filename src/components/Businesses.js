b1737355aa872840fcffc34b02e92a442a41b7a1
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-07-20 16:09:40 -0700

    refactor to functional component

--- src/components/Businesses.js
+++ src/components/Businesses.js
@@ -1,49 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {connect} from 'react-redux';
 import * as a from '../actions/index';
 
-class Businesses extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+function Businesses(props) {
+  useEffect(() => {
+   const {dispatch} = props;
+   const action = a.makeApiCall();
+   dispatch(action);
+  }, [])
 
-  componentDidMount() {
-   const {dispatch} = this.props;
-  //  const action1 = a.requestBusinesses();
-  //  dispatch(action1);
-   const action2 = a.makeApiCall();
-   dispatch(action2);
-  }
-
-  render() {
-    const { error, isLoaded, businesses } = this.props;
-    if (error) {
-      return (
-        <React.Fragment>
-          Error: {error.message}
-        </React.Fragment>
-      )
-    } else if (!isLoaded) {
-      return (
-        <React.Fragment>
-          Loading...
-        </React.Fragment>
-      )
-    } else {
-      return (
-        <React.Fragment>
-          <h1>Businesses</h1>
-          <ul>
-            {businesses.map((business, index) =>
-              <li key={index}>
-                <h3>{business.name}</h3>
-                <p>{business.address}</p>
-              </li>
-            )}
-          </ul>
-        </React.Fragment>
-      )
-    }
+  const { error, isLoaded, businesses } = props;
+  if (error) {
+    return (
+      <React.Fragment>
+        Error: {error.message}
+      </React.Fragment>
+    )
+  } else if (!isLoaded) {
+    return (
+      <React.Fragment>
+        Loading...
+      </React.Fragment>
+    )
+  } else {
+    return (
+      <React.Fragment>
+        <h1>Businesses</h1>
+        <ul>
+          {businesses.map((business, index) =>
+            <li key={index}>
+              <h3>{business.name}</h3>
+              <p>{business.address}</p>
+            </li>
+          )}
+        </ul>
+      </React.Fragment>
+    )
   }
 }
 
