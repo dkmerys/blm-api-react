c8dd4594e91c76fe84a9a3bc3ae374cdd3c386ce
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-07-21 10:41:14 -0700

    add new components, troubleshoot local api connection

---
+++ src/components/BusinessControl.js
@@ -0,0 +1,29 @@
+// import React from "react";
+// import { connect } from "react-redux";
+// import PropTypes from "prop-types";
+// import * as a from "./../actions/index"
+// import Business from "./Businesses";
+// import NewBusinessForm from "./NewBusinessForm";
+// import EditBusinessForm from "./EditBusinessForm";
+// import BusinessDetail from "./BusinessDetail";
+
+// function BusinessControl (props) {
+//   const { newBusinessFormVisible, editBusinessFormVisible, selectedBusiness } = props;
+
+//   return (
+//     <React.Fragment>
+
+//     </React.Fragment>
+//   )
+
+// }
+
+// const mapStateToProps = state => {
+//   return{
+//     newBusinessFormVisible: state.newBusinessFormVisible,
+//     editBusinessFormVisible: state.editBusinessFormVisible,
+//     selectedBusiness: state.selectedBusiness
+//   }
+// }
+
+// BusinessControl = connect(mapStateToProps)(BusinessControl);
